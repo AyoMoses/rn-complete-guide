c078f03309b16b1a02f9b7b57bea824fec405b60
odukoyaayodeji@gmail.com <odukoyaayodeji@gmail.com>
2019-11-13 14:58:16 +0100

    data, key/id, scrollView, FlatList, Hooks(useState)

--- App.js
+++ App.js
@@ -1,15 +1,62 @@
-import React from "react";
-import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  ScrollView,
+  FlatList
+} from "react-native";
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = enteredText => {
+    setEnteredGoal(enteredText);
+  };
+
+  // DISPLAY OUR ENTERED GOAL
+  const addGoalHandler = () => {
+    setCourseGoals(currentGoals => [
+      ...currentGoals,
+      { key: Math.random().toString(), value: enteredGoal }
+    ]);
+  };
+
+  // function goalInputHandler(enteredText) {
+  //   setEnteredGoal(enteredText);
+  // };
+  // // USING ONE OF THE TWO WORKS
+  // const goalInputHandler = (enteredText) => {
+  //   setEnteredGoal(enteredText);
+  // };
+
+  // ANOTHER FUNCTION FOR WATCHING WHEN THE ADD BUTTON IS CLIICKED
+
   return (
     <View style={styles.screenWrap}>
       <View style={styles.inputContainer}>
-        <TextInput placeholder="Course Goal" style={styles.formInput} />
-        <Button title="Click to Add" style={{}} />
+        <TextInput
+          placeholder="Course Goal"
+          style={styles.formInput}
+          onChangeText={goalInputHandler}
+          // WE PASS THE USER KEYPRESS INTO THE INPUTTEXT BY BINDING DATA ONE WAY
+          value={enteredGoal}
+        />
+        {/* WE THEN BIND THE GOALINPUT HANDLER */}
+        <Button title="Click to Add" style={{}} onPress={addGoalHandler} />
       </View>
 
-      <View></View>
+      <FlatList
+        data={courseGoals}
+        renderItem={itemData => (
+          <View style={styles.listItem}>
+            <Text>{itemData.item.value}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 }
@@ -24,7 +71,7 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center"
-  }, 
+  },
 
   formInput: {
     width: 220,
@@ -32,5 +79,11 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     padding: 7,
     marginRight: 5
+  },
+
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: "#ddd"
   }
 });
